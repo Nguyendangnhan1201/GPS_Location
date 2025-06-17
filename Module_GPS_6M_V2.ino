204c4e696033dde9d38891f70693e8db42db4ba5
Nguyễn Đặng Nhân <nhannguyendang1201@gmail.com>
2025-06-17 13:05:09 +0700

    Update Module_GPS_6M_V2.ino

--- Module_GPS_6M_V2.ino
+++ Module_GPS_6M_V2.ino
@@ -16,10 +16,11 @@ void setup(void) {
   lcd.init();
   lcd.backlight( );
   lcd.clear();
+  // Make an introduction for fun:
   lcd.setCursor(0, 0);
   lcd.print("This is GPS by");
   lcd.setCursor(0, 1);
-  lcd.print("Nguyen Dang Nhan");
+  lcd.print("Your name");
   delay(2000);
   lcd.clear();
   lcd.setCursor(0, 0);
